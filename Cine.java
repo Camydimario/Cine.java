10f5148dc4d0baac576b93015d44b1454b633622
Camy Di Mario <166224855+Camydimario@users.noreply.github.com>
2026-06-12 21:57:02 -0300

    Create Cine.java

---
+++ Cine.java
@@ -0,0 +1,84 @@
+//dada una clase cine se desea armar un programa que realize diferentes operaciones y devuelva lo que se necesita
+
+/** se crea una clase Cine que tiene como atributos un arrays de asientos int [][] asientos.
+**/
+
+public class Cine (){}
+
+// pre: los numeros de asientos no pueden ser negativos, todos deben ser mayores a cero
+public Cine(){}
+
+//post: devuelve la suma total de todos los asientos
+public int sumarTodos(){}
+
+//post:  devuelve la suma de cada fila de asientos
+public int [] sumarFilas(){}
+
+//post : devuleve la suma de cada columna de asientos
+public int [] sumarColumnas(){}
+
+//pre : se calcula el promedio de los asientos, un promedio nunca puede ser negativo
+//post: devuleve el promedio total de asientos
+public double promediarTodos(){}
+
+//post: se promedian las filas 
+public double [] promediarFilas(){}
+
+//post : se promedian las columnas
+public double [] promediasColumnas(){}
+
+// pre: los promedios tienen un numero , pero se intenta saber la cantidad de aseintos que estan por ensima del promedio
+//post : devuleven la cantidad de asientos que estan por ensima del promedio
+
+public int ensimaDelPromedio (){}
+
+//pre: los numeros de asientos deben ser mayores a cero, nunca pueden ser negativos
+// post: devuelve la cantidad de asientos del cine 
+
+public int cantidadDeAsientos(){}
+
+// post: devueleve la cantidad de filas del cine 
+public int cantidadDeFilas(){}
+
+//post: devuelve la cantidad de Columnas del cine
+public int cantidadDeCOlumnas(){} 
+  
+//pre: se calcula la suma de todos los asientos que tiene el cine, pero se ignora el asiento con el numero mas alto ya que ese es reserbao
+//post : se devuelve la suma  de los asinentos menos el maximo
+
+public int SumarTodosMenosElMinimos(){}
+
+//post: devolver la cantidad de asientos continuos
+
+public int CantidadDeAsientosContinuos(){}
+
+//post : devolver la posicion del asiento con el numero mas bajo
+public int posicionDelMasBajo(){}
+
+//post: devolver el asiento con el numero minimo
+public int minimo(){}
+
+//post: devolver el asiento  con el nnumero maximo
+public int maximo(){}
+
+//post: devolver la posicion del anteUtimo asiento del cine
+
+public int PosAnteUlitmo(){}
+
+// post: verifica si el cine es cuadrada
+
+public boolean esCuadrada(){}
+
+
+public static void main (String [] args){
+  Cine cinesito = new Cine (New int [] = {};
+
+// realizar las preubas correspondiente
+
+
+
+
+
+
+
+
